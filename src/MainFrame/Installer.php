b86a81d1929e300b51cc6f967b83423f3526d5dc
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-04-03 21:55:39 +0300

    -

--- src/MainFrame/Installer.php
+++ src/MainFrame/Installer.php
@@ -5,6 +5,32 @@ namespace App\Modules\MainFrame;
  
 class Installer
 {
+
+    private static function _copyOrSymlink($mode, $pathFrom, $pathTo, $fileFrom, $fileTo): void 
+    {
+        print_r('Копируем '.$mode.' '.$pathFrom.' '.$pathTo.' '.$fileFrom.' '.$fileTo."\n");
+        if(!file_exists($pathFrom.$fileFrom)) {
+            print_r('Файл '.$pathFrom.$fileFrom.' не существует'."\n");
+            return;
+        }
+
+        if(file_exists($pathTo.$fileTo)) {
+            print_r('Файл '.$pathTo.$fileTo.' существует'."\n");
+            return;
+        }
+
+        if($mode === 'local') {
+            shell_exec('ln -s '.realpath($pathFrom.$fileFrom).' '.$pathTo.($fileTo != $fileFrom ? $fileTo : ''));
+        }
+        else {
+            shell_exec('cp -R '.realpath($pathFrom.$fileFrom).' '.$pathTo.$fileTo);
+        }
+
+        // если это исполняемый скрипт
+        if(strstr($pathTo.$fileTo, '/bin/') !== false) {
+            chmod($pathTo.$fileTo, 0777);
+        }
+    }
  
     /**
      *
@@ -34,33 +60,22 @@ class Installer
         $installedPackage = $operation->getPackage();
         $targetDir = $installedPackage->getName();
         $path = $vendorDir.$targetDir;
+        $configPath = $path.'/src/MainFrame/config-template/';
  
         // копируем конфиг
         print_r('Копируем файл конфигурации'."\n");
-        $configPath = $path.'/src/MainFrame/config-template/module-'.$mode.'.yaml';
-        $configTargetPath = $configDir.'mainframe.yaml';
-        if(file_exists($configTargetPath)) {
+        if(file_exists($configDir.'mainframe.yaml')) {
             print_r('Файл конфигурации найден, пропускаем настройку'."\n");
             return;
         }
-        if($mode === 'local') {
-            symlink($configPath, $configTargetPath);
-        }
-        else {
-            copy($configPath, $configTargetPath);
-        }
-        $configPath = $path.'/src/MainFrame/config-template/mainframe-menu.yaml';
-        $configTargetPath = $configDir.'mainframe-menu.yaml';
-        if(file_exists($configTargetPath)) {
+        self::_copyOrSymlink($mode, $configPath, $configDir, 'module-'.$mode.'.yaml', 'mainframe.yaml');
+
+        if(file_exists($configDir.'mainframe-menu.yaml')) {
             print_r('Файл конфигурации найден, пропускаем настройку'."\n");
             return;
         }
-        if($mode === 'local') {
-            symlink($configPath, $configTargetPath);
-        }
-        else {
-            copy($configPath, $configTargetPath);
-        } 
+        self::_copyOrSymlink($mode, $configPath, $configDir, 'mainframe-menu.yaml', 'mainframe-menu.yaml');
+
         // нужно прописать в модули
         $modulesTargetPath = $configDir.'modules.yaml';
         $modulesConfigContent = file_get_contents($modulesTargetPath);
@@ -80,23 +95,13 @@ class Installer
         $scriptsPath = $path.'/src/MainFrame/bin/';
         $binDir = './bin/';
  
-        if($mode === 'local') {
-            symlink($scriptsPath.'mainframe-migrate.sh', $binDir.'mainframe-migrate.sh');
-        }
-        else {
-            copy($scriptsPath.'mainframe-migrate.sh', $binDir.'mainframe-migrate.sh');
-        }
+        self::_copyOrSymlink($mode, $scriptsPath, $binDir, 'mainframe-migrate.sh', 'mainframe-migrate.sh');
         print_r('Копирование изображений'."\n");
 
         $sourcePath = $path.'/src/MainFrame/web/res/img/';
         $targetDir = './web/res/img/';
 
-        if($mode === 'local') {
-            symlink($sourcePath.'mainframe-logo-colibri.svg', $targetDir.'mainframe-logo-colibri.svg');
-        }
-        else {
-            copy($sourcePath.'mainframe-logo-colibri.svg', $targetDir.'mainframe-logo-colibri.svg');
-        } 
+        self::_copyOrSymlink($mode, $sourcePath, $targetDir, 'mainframe-logo-colibri.svg', 'mainframe-logo-colibri.svg');
         print_r('Установка завершена'."\n");
  
     }
