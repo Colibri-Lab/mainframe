1eb9e7e954c25e81ecff7281795ea677947bb393
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-07-25 18:21:19 +0300

    -

--- src/MainFrame/Installer.php
+++ src/MainFrame/Installer.php
@@ -105,10 +105,6 @@ class Installer
         if(!$webRoot) {
             $webRoot = 'web'; 
         }
-        $permanent = \getenv('COLIBRI_PERMANENT');
-        if(!$permanent) {
-            $permanent = 'res/'; 
-        }
         $mode = self::_getMode($configDir.'app.yaml');
 
         // копируем конфиг
