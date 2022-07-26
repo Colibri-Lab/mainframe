ed236267399c0d35157b6672735400e32303797e
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-07-26 21:13:06 +0300

    -

--- src/MainFrame/Controllers/Controller.php
+++ src/MainFrame/Controllers/Controller.php
@@ -116,10 +116,10 @@ class Controller extends WebController
 
         });
 
-        $jsBundle = Bundle::Automate('assets.bundle.js', 'js', [
+        $jsBundle = Bundle::Automate(App::$domainKey, 'assets.bundle.js', 'js', [
             ['path' => App::$moduleManager->MainFrame->modulePath . '.Bundle/', ['exts' => ['js', 'html']]],
         ]);
-        $cssBundle = Bundle::Automate('assets.bundle.css', 'scss', array(
+        $cssBundle = Bundle::Automate(App::$domainKey, 'assets.bundle.css', 'scss', array(
             ['path' => App::$moduleManager->MainFrame->modulePath . '.Bundle/'],
         ));
 
