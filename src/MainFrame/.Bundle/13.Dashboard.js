5ab0f654293fe0f0390eca952157ea2806a7b0fb
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-02-20 11:06:07 +0300

    -

--- src/MainFrame/.Bundle/13.Dashboard.js
+++ src/MainFrame/.Bundle/13.Dashboard.js
@@ -6,13 +6,68 @@ App.Modules.MainFrame.Dashboard = class extends Colibri.UI.Pane {
         this.AddClass('app-modules-mainframe-dashboard');
 
         this._container = this.Children('container');
-        this._loadWidgets();
+        const draggable = this._loadWidgets();
 
+        this._dragManager = new Colibri.UI.DragManager(draggable, [this._container]);
+        this._dragManager.AddHandler('DragDropComplete', (event, args) => this.__dragDropComplete(event, args));
+        this._dragManager.AddHandler('DragDropOver', (event, args) => this.__dragDropOver(event, args));
+        this._dragManager.AddHandler('DragDropLeave', (event, args) => this.__dragDropLeave(event, args));
+
+    }
+
+    __dragDropLeave(event, args) {
+        const dragged = args.dragged;
+        const droppedTo = args.droppedTo;
+        const droppedToElement = args.droppedToElement;
+        const widgetElement = droppedToElement.closest('.app-component-widget');
+        if(widgetElement) {
+            const droppedToWidget = widgetElement.tag('component');
+            droppedToWidget.RemoveClass('drag-over')
+        }
+    }
+
+    __dragDropOver(event, args) {
+        const dragged = args.dragged;
+        const droppedTo = args.droppedTo;
+        const droppedToElement = args.droppedToElement;
+        
+        const widgetElement = droppedToElement.closest('.app-component-widget');
+        if(widgetElement) {
+            const droppedToWidget = widgetElement.tag('component');
+            droppedTo.ForEach((name, component) => component.RemoveClass('drag-over'));
+            droppedToWidget.AddClass('drag-over');
+            console.log(dragged, droppedTo, droppedToWidget);
+        }    
+    }
+
+    __dragDropComplete(event, args) {
+        const dragged = args.dragged;
+        const droppedTo = args.droppedTo;
+        const droppedToElement = args.droppedToElement;
+        
+        const widgetElement = droppedToElement.closest('.app-component-widget');
+        if(widgetElement) {
+
+            const droppedToWidget = widgetElement.tag('component');
+            droppedToWidget.RemoveClass('drag-over');
+            droppedTo.Children(dragged.name, dragged, droppedToWidget.index, droppedTo.container, dragged.mainElement);
+
+            this._saveWidgetsPositions();
+
+        }
+    }
+
+    _saveWidgetsPositions() {
+        let positions = [];
+        this._container.ForEach((name, widget) => {
+            positions.push(name);
+        });
+        App.Browser.Set('dashboard-widgets', positions);
     }
 
     _loadWidgets() {
 
-        const widgets = [];
+        let widgets = [];
         Object.forEach(App.Modules, (name, module) => {
             try {
                 const moduleWidgets = eval('App.Modules.' + name + '.Widgets');
@@ -27,26 +82,45 @@ App.Modules.MainFrame.Dashboard = class extends Colibri.UI.Pane {
             }
         });
 
-        widgets.sort((a, b) => {
-            if(a.Params().defaultIndex > b.Params().defaultIndex) {
-                return 1;
-            } else if(a.Params().defaultIndex < b.Params().defaultIndex) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
+        const positions = App.Browser.Get('dashboard-widgets').split(',');
+        if(!positions) {
 
+            widgets.sort((a, b) => {
+                if(a.Params().defaultIndex > b.Params().defaultIndex) {
+                    return 1;
+                } else if(a.Params().defaultIndex < b.Params().defaultIndex) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+
+        } else {
+
+            const w = [];
+            for(const name of positions) {
+                for(const widget of widgets) {
+                    if(widget.Params().name == name) {
+                        w.push(widget);
+                    }
+                }
+            }
+            widgets = w;
+        }
+
+
+        const ret = [];
         for(const widget of widgets) {
-            const widgetInstance = new widget(widget.Params().name + '-widget', this._container);
+            const widgetInstance = new widget(widget.Params().name, this._container);
             if(!(widgetInstance instanceof Colibri.UI.Widget)) {
                 widgetInstance.Dispose();
             } else {
                 widgetInstance.shown = true;
+                ret.push(widgetInstance);
             }
         }
 
-        
+        return ret;
 
     }
 
